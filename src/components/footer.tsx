dd2187982df3f396aba0685e4867e8e651579e25
Sw1ndlers <lisina2023@icloud.com>
2025-04-10 18:34:17 -0700

    Footer responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,8 @@ import { GithubIcon, InstagramIcon, MailIcon } from "lucide-react";
 
 export default function Footer() {
     return (
-        <footer className="w-full h-54 bg-white p-10">
-            <div className="flex flex-row h-full justify-between">
+        <footer className="w-full  h-54 bg-white py-10 px-10 md:px-20 flex justify-center">
+            <div className="flex flex-row h-full justify-between  w-full">
                 <div className=" h-full flex flex-col">
 
                     <p className=" text-primary font-bold text-3xl"> Supplied Scholars </p>
@@ -17,8 +17,8 @@ export default function Footer() {
                     </div>
                 </div>
 
-                <div className=" flex flex-row h-full gap-12">
-                    <div>
+                <div className=" flex flex-row h-full gap-12 mt-0.5">
+                    <div className="">
                         <p className=" font-bold text-primary text-xl"> About Us </p>
                         <div className=" text-neutral-900 mt-4 text-md flex flex-col gap-1">
                             <p > Our Mission </p>
@@ -27,7 +27,7 @@ export default function Footer() {
                         </div>
                     </div>
 
-                    <div >
+                    <div className="hidden sm:block">
                         <p className=" font-bold text-primary text-xl"> Get Involved </p>
                         <div className=" text-neutral-900 mt-4 text-md flex flex-col gap-1">
                             <p > Donate </p>
